88e10ed703356e37716d11a6967eaef742c4b243
Sandesh Jangir <sandeshatgithub@gmail.com>
2023-03-24 21:51:20 +0530

    Recursions With Array

--- Recursion.cpp
+++ Recursion.cpp
@@ -10,6 +10,14 @@ int factorial(int n)
         return (n * factorial(n-1)); // Here the whole product is self work and the factorial(n-1) is assumption. 
     }
 }
+
+// Recursion on array
+void print_array(int *arr, int index, int n){
+    if (index < n){
+        cout << arr[index] << " ";
+        print_array(arr, index+1, n);
+    }
+}
 int main()
 {
     /**
@@ -24,9 +32,19 @@ int main()
      * Base Case - It is the value whose result is already known when applied into the formula.
      * Assumption - It is the value that is assumed to work perfectly fine with the formula.
      * Self Work - The process to prove the formula with the help of base case and assumption.
+     * 
+     *## Time Complexity of a recursive function = no. of times the function is called * time consumed by 1 function call.
+     * 
+     * Recursions on arrays and strings - C++
+     * 
+     * To solve a problem related to arrays and strings using recursion, most of the times we would have to read the array. Therefore majority of times our function would look like this -> func (arr, index, array_length ....){ function_operations }
      */
 
     // Let's Take an example of a factorial function.
     cout << factorial(5) << endl;
+
+    // Printing an array using recursion.
+    int arr[] = {1,2,3,4,5,10};
+    print_array(arr, 0, 6);
     return 0;
 }
